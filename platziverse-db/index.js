f1607778267def408a1af334288e0551fda0d226
andresFMedina <andres.medina@correounivalle.edu.co>
2019-06-06 21:35:07 -0500

    iniciacion

---
+++ platziverse-db/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const setupDatabase = require('./lib/db');
+const setupAgentModel = require('./models/agent');
+const setupMetricModel = require('./models/metric');
+
+module.exports = async function (config) {
+    const sequelize = setupDatabase(config);
+    const AgentModel = setupAgentModel(config);
+    const MetricModel = setupMetricModel(config);
+
+    AgentModel.hasMany(MetricModel);
+    MetricModel.belongsTo(AgentModel);
+
+    await sequelize.authenticate();
+
+    if(config.setup){
+        await sequelize.sync({force: true}) //force: true --> en caso de que la db exista la crea otra vez borrando todo        
+    }
+
+    //sequelize.sync();
+
+    const Agent = {}
+    const Metric = {}
+
+    return {
+        Agent,
+        Metric
+    }
+}
